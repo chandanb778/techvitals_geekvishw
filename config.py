2b04564ffab9fd590443466073904c1292c57df7
chandanb778 <chandanbhirud77@gmail.com>
2025-02-11 00:27:07 +0530

    config changed

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ import streamlit as st
 SCOPES = [
     'https://www.googleapis.com/auth/fitness.activity.read',
     'https://www.googleapis.com/auth/fitness.heart_rate.read',
-    'https://www.googleapis.com/auth/fitness.sleep.read'
+    'https://www.googleapis.com/auth/fitness.sleep.read',
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email", 
+    "https://www.googleapis.com/auth/userinfo.profile"
 ]
 
 def get_credentials():
@@ -24,12 +27,13 @@ def get_credentials():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', 
                 SCOPES,
-                redirect_uri='http://127.0.0.1:8501/oauth2callback'
+                redirect_uri='https://techvital.streamlit.app/'
+                # redirect_uri='http://127.0.0.1:8501/oauth2callback'
             )
             
             # Use local server with specific port
             creds = flow.run_local_server(
-                host='127.0.0.1',
+                host='0.0.0.0',
                 port=8501,
                 authorization_prompt_message='Please authorize the application',
                 success_message='Authentication successful! You can close this window and return to the app.',
